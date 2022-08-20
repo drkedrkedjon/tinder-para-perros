368805d0773537db0cf70a278732df04d34acfcc
Sasa Savic Perusina <sasa@bubulazy.com>
2022-08-20 15:21:14 +0100

    HTML y CSS grid modificados, falta JS

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 // Remember to import the data and Dog class!
-console.log('sasa')
+
+// {
+//   name: "Simba",
+//   avatar: "images/dog-simba.jpg",
+//   age: 21,
+//   bio: "!Que pasa tio!",
+//   hasBeenSwiped: false,
+//   hasBeenLiked: false
+// }
+
+class Perros {
+  constructor() {
+
+  }
+}
